1e0c6d7f35885d929b8c2efd04842546b0f16449
Philippe Miguet <philippe@bufferapp.com>
2018-11-22 16:49:08 +0100

    Add support for staging env and customized redirect urls (#1)
    * Add support for staging env and customized redirect urls
    
    * Rely on environment variables instead
    
    * fix lint issue
    
    * Create a beta version
    
    * Revert "Create a beta version"
    
    This reverts commit 0da6134398a80943f4673373f88e8a5636e704bb.
    
    * Security: update jest to fix vulnerability in merge package
    
    * Add new ENV variables to the README

--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 
 Buffer session manager
 
+## Configuration via environment variables
+
+### `SESSION_SERVICE_REDIRECT_URL` (optional)
+
+* Defines the location for the redirect response for invalid sessions.
+* Default value is `https://login.buffer.com` in production and `https://login.local.buffer.com` in development
+### `SESSION_SERVICE_URL` (optional)
+
+* Defines the internal URL to the `session-service`.
+* Defaults to `http://session-service-1.core` in production and `http://session-service-1:3000` in development.
+
 ## Env Presets
 
 Targets node and browsers with > 3% market share. See this tweet for more context:
--- src/urls.js
+++ src/urls.js
@@ -1,11 +1,21 @@
 const currentWindowUrl = () => window.location.href
 
-export const loginServiceUrl = ({ production }) =>
-  `https://login${production ? '' : '.local'}.buffer.com`
+export const loginServiceUrl = ({ production }) => {
+  if (!process.env.SESSION_SERVICE_REDIRECT_URL)
+    return `https://login${production ? '' : '.local'}.buffer.com`
+
+  return process.env.SESSION_SERVICE_REDIRECT_URL
+}
 
 export const logoutUrl = ({ production }) =>
   `${loginServiceUrl({ production })}/logout/?redirect=${currentWindowUrl()}`
 
 // TODO: remove beta '1' version after its been removed
-export const sessionServiceUrl = ({ sessionVersion = '1', production }) =>
-  `http://session-service-${sessionVersion}${production ? '.core' : ':3000'}`
+export const sessionServiceUrl = ({ sessionVersion = '1', production }) => {
+  if (!process.env.SESSION_SERVICE_URL)
+    return `http://session-service-${sessionVersion}${
+      production ? '.core' : ':3000'
+    }`
+
+  return process.env.SESSION_SERVICE_URL
+}
